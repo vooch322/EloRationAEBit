d8a05f1bdef87c2ee0aad81db3ecfe0e37d303d9
vooch <vooch94@gmail.com>
2018-07-27 09:47:57 +0900

    v0.1

--- EloRationAEBit/Form1.Designer.cs
+++ EloRationAEBit/Form1.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.RatioAfter1 = new System.Windows.Forms.Label();
+            this.RatioAfter2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Рейтинг1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Рейтинг2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Голы1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Голы2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(368, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // RatioAfter1
+            // 
+            this.RatioAfter1.AutoSize = true;
+            this.RatioAfter1.Location = new System.Drawing.Point(104, 231);
+            this.RatioAfter1.Name = "RatioAfter1";
+            this.RatioAfter1.Size = new System.Drawing.Size(35, 13);
+            this.RatioAfter1.TabIndex = 9;
+            this.RatioAfter1.Text = "label5";
+            // 
+            // RatioAfter2
+            // 
+            this.RatioAfter2.AutoSize = true;
+            this.RatioAfter2.Location = new System.Drawing.Point(368, 231);
+            this.RatioAfter2.Name = "RatioAfter2";
+            this.RatioAfter2.Size = new System.Drawing.Size(35, 13);
+            this.RatioAfter2.TabIndex = 10;
+            this.RatioAfter2.Text = "label6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Группа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(266, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Верхняя";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(347, 317);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "нижняя";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 418);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.RatioAfter2);
+            this.Controls.Add(this.RatioAfter1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label RatioAfter1;
+        private System.Windows.Forms.Label RatioAfter2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- EloRationAEBit/Form1.cs
+++ EloRationAEBit/Form1.cs
@@ -16,5 +16,110 @@ namespace EloRationAEBit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ratio1, ratio2;
+            int score1, score2;
+            ratio1 = Convert.ToInt32(textBox1.Text);
+            ratio2 = Convert.ToInt32(textBox2.Text);
+            score1 = Convert.ToInt32(textBox3.Text);
+            score2 = Convert.ToInt32(textBox4.Text);
+            if (score1 > score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1,ratio2,1,50));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0,50));
+            }
+            if (score1 < score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0,50));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 1,50));
+            }
+            if(score1 == score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0.5,50));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0.5,50));
+            }
+        }
+        private double ELO(double ratio1,double ratio2,double Sa,double k)
+        {
+            double E = 1 / (1 + Math.Pow(10, (ratio2 - ratio1)/400));
+            return Math.Round(ratio1 + k * (Sa - E));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double ratio1, ratio2;
+            int score1, score2;
+            ratio1 = Convert.ToInt32(textBox1.Text);
+            ratio2 = Convert.ToInt32(textBox2.Text);
+            score1 = Convert.ToInt32(textBox3.Text);
+            score2 = Convert.ToInt32(textBox4.Text);
+            if (score1 > score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 1,20));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0,20));
+            }
+            if (score1 < score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0,20));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 1,20));
+            }
+            if (score1 == score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0.5,20));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0.5,20));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double ratio1, ratio2;
+            int score1, score2;
+            ratio1 = Convert.ToInt32(textBox1.Text);
+            ratio2 = Convert.ToInt32(textBox2.Text);
+            score1 = Convert.ToInt32(textBox3.Text);
+            score2 = Convert.ToInt32(textBox4.Text);
+            if (score1 > score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 1,70));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0,70));
+            }
+            if (score1 < score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0,70));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 1,70));
+            }
+            if (score1 == score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0.5,70));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0.5,70));
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double ratio1, ratio2;
+            int score1, score2;
+            ratio1 = Convert.ToInt32(textBox1.Text);
+            ratio2 = Convert.ToInt32(textBox2.Text);
+            score1 = Convert.ToInt32(textBox3.Text);
+            score2 = Convert.ToInt32(textBox4.Text);
+            if (score1 > score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 1,40));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0,40));
+            }
+            if (score1 < score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0,40));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 1,40));
+            }
+            if (score1 == score2)
+            {
+                RatioAfter1.Text = Convert.ToString(ELO(ratio1, ratio2, 0.5,40));
+                RatioAfter2.Text = Convert.ToString(ELO(ratio2, ratio1, 0.5,40));
+            }
+        }
     }
 }
